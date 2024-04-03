c516ec2dd37404c7e5c9fe7cfe7ada833c2dcd11
YatinDevs <c.yatin727@gmail.com>
2024-04-03 14:24:54 +0530

    feat : Add Load Component (updates Image Upload with S3)

--- src/pages/Load/AddLoad/ImageUpload.jsx
+++ src/pages/Load/AddLoad/ImageUpload.jsx
@@ -83,9 +83,9 @@ function ImageUpload() {
           </p>
         </div>
         {source && (
-          <div className=" px-1  py-1 bg-gray-100 rounded-xl">
+          <div className="mt-2 px-1  py-1 bg-gray-100 rounded-xl">
             <div
-              className="w-full h-[150px] my-[10px] bg-cover bg-center  bg-no-repeat border  border-gray-300 border-5 rounded-lg cursor-pointer"
+              className="w-full h-[150px]  bg-cover bg-center  bg-no-repeat border  border-gray-300 border-5 rounded-lg cursor-pointer"
               style={{ backgroundImage: `url(${source})` }}
             ></div>
           </div>
